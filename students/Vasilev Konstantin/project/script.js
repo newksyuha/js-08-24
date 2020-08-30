a5fb9ce4c28e3755fa3ece0c42057b95c09503c6
adoreducks <58430903+adoreducks@users.noreply.github.com>
2020-08-30 15:37:37 +0400

    hw1
    added .join

--- students/Vasilev Konstantin/project/script.js
+++ students/Vasilev Konstantin/project/script.js
@@ -5,13 +5,13 @@ const goods = [
     { title: 'Shoes', price: 250 },
 ];
 
-const renderGoodsItem = (title = "No title", price = "No price") => {
+const renderGoodsItem = (title = "No title", price = "No price ") => {
     return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>` ;
 };
 
 const renderGoodsList = (list) => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList;
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
 
 renderGoodsList(goods);
