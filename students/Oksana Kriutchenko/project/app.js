a99eb1c36aee414da6dd2ff121aabb94727f67d6
Ksu <newksyuha@gmail.com>
2020-08-31 13:35:52 +0300

    Home Work lesson2

--- students/Oksana Kriutchenko/project/app.js
+++ students/Oksana Kriutchenko/project/app.js
@@ -4,47 +4,136 @@ const goods = [
     {
         //        title: 'Honda NC 750',
         img: 'img/honda_NC_750_XD_2018.jpg',
-        price: 250000,
+        price: 12000,
     },
     {
         title: 'Honda New CB500X',
         img: 'img/honda_CB500X.jpg',
-        //        price: 250000
+        //        price: 8500
     },
     {
         title: 'NC 750 S-1',
         img: 'img/NC_750_S-1.jpg',
-        price: 280000,
+        price: 10500,
     },
     {
         title: 'NC 750 XD 2018',
         //        img: 'img/Honda_NC_750_XD_2018-4.jpg',
-        price: 350000,
+        price: 11000,
     },
 ];
 
-const renderGoodsItem = (title = "Что-то пошло не так c title!", img = "Что-то пошло не так c img!", price = "Что-то пошло не так c price!") => {
-    return `<div class="goods-item"><h3>${title}</h3><div><img src=${img} width="250px" height="200px"></div><p>${price}</p><button class="btn">Купить</button></div>`;
+class GoodsItem {
+    constructor({
+        title,
+        img,
+        price
+    }) {
+        this.title = title;
+        this.img = img;
+        this.price = price;
+    }
+    render() {
+        return `
+            <div class="goods-item">
+                <h3>${this.title}</h3>
+                <div><img src=${this.img} width="250px" height="200px"></div>
+                <p>${this.price}USD</p>
+                <button class="btn">Купить</button>
+            </div>
+        `;
+    }
 };
 
-const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.img, item.price)).join('');
-    document.querySelector('.goods-list').innerHTML = goodsList;
+class GoodsList {
+    constructor() {
+        this.goods = [];
+        this.fetchGoods();
+    }
+    fetchGoods() {
+        this.goods = [
+            {
+                title: 'Honda NC 750',
+                img: 'img/honda_NC_750_XD_2018.jpg',
+                price: 12000
+    },
+            {
+                title: 'Honda New CB500X',
+                img: 'img/honda_CB500X.jpg',
+                price: 8500
+    },
+            {
+                title: 'NC 750 S-1',
+                img: 'img/NC_750_S-1.jpg',
+                price: 10500
+    },
+            {
+                title: 'NC 750 XD 2018',
+                img: 'img/Honda_NC_750_XD_2018-4.jpg',
+                price: 11000
+    },
+];
+    }
+    render() {
+        const goodsList = this.goods.map(item => {
+            const goodsItem = new GoodsItem(item);
+            return goodsItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = goodsList.join('');
+    }
+
+
+//Метод подсчета суммарной стоимости продуктов
+
+    getTotalSumProduct() {
+        let totalSum = 0;
+        for (let product of this.goods) {
+            totalSum += product.price;
+        }
+        console.log(totalSum);
+        return totalSum;
+    }
+
+    renderTotalSumProduct() {
+        const blockSum =
+            document.querySelector('.btn');
+         blockSum.insertAdjacentHTML('afterend', `<br><p>Стоимость всех продуктов</p>${this.getTotalSumProduct()}`);
+    }
 }
+const goodsList = new GoodsList();
+goodsList.render();
+goodsList.renderTotalSumProduct();
 
-renderGoodsList(goods);
 
-//'use strict';
-//
-//const renderGoodsItem = (title, price) => {
-//    return (`<div class="goodsAndButton"><div class="goods-item"><h3>${title}</h3><p>${price} USD</p></div>
-//            <div><img src='https://placehold.it/150x100'><img></div>
-//           <div><button class="sale-button" type="button">В корзину</button></div></div>`)
-//};
-//
-//const renderGoodsList = (list) => {
-//    let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-//    document.querySelector('.goods-list').innerHTML = goodsList;
-//};
-//
-//renderGoodsList(goods);
+
+//Создаем пустые классы для Корзины товаров и Элемента корзины товаров
+
+class cartButton {
+    constructor(){
+        
+    }
+
+    clearCartButton(){
+        //метод, который производит очистку корзины
+    }
+    sumCartButton(){
+        // метод, который считает сумму товаров в корзине
+    }
+    payCartButton(){
+        //метод, который выбирает форму оплаты
+    }
+}
+class elementCartButton {
+    constructor(){
+        //price     цена
+        //quantity  количество
+        //name      наименование
+    }
+    addElementCartButton(){
+        //метод, который добавляет товар в корзину
+    }
+
+    deleteElementCartButton(){
+        //метод удаляющий элемент с корзины
+    }
+}
