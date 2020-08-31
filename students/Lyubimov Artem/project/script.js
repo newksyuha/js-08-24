40fcce0144bdfed9abbd231e790baf1e4c5d553c
Timon4eg <62251008+Timon4eg@users.noreply.github.com>
2020-08-31 10:59:32 +0300

    Home work 2

--- students/Lyubimov Artem/project/script.js
+++ students/Lyubimov Artem/project/script.js
@@ -1,18 +1,78 @@
-const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-  ];
+class GoodsItem {
+    constructor({title, price}) {
+        this.title = title
+        this.price = price
+    }
 
-const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-};
-  
-const renderGoodsList = (list) => {
-    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
-    document.querySelector('.goods-list').innerHTML = goodsList;
+    render() {
+        return `
+        <div class="goods-item">
+            <h3>${this.title}</h3>
+            <p>${this.price}</p>
+        </div>
+        `
+    }
 }
-  
-renderGoodsList(goods);
+
+class GoodsList {
+    constructor() {
+        this.goods = []
+        this.fetchGoods()
+    }
+
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ]
+    }
+
+    getSum() {
+        const sum = this.goods.reduce((sum, good) => {
+            return sum + good.price
+        }, 0)
+        const sumBlock = document.createElement('div')
+        sumBlock.classList.add('cart-sum')
+        sumBlock.innerText = `Сумма товаров в списке: ${sum}`
+        console.log(sumBlock)
+    }
+
+    render() {
+        const goodsList = this.goods.map(item => {
+            const goodsItem = new GoodsItem(item)
+            return goodsItem.render()
+        })
+        document.querySelector('.goods-list').innerHTML = goodsList.join('')
+    }
+}
+
+class CartItem {
+    constructor() {
+
+    }
+
+    plusCartItem() {
+
+    }
+
+    minusCartItem() {
+        
+    }
+}
+
+class CartList {
+    constructor() {
+
+    }
+
+    render() {
+        
+    }
+}
+
+const goodsList = new GoodsList()
+goodsList.render()
+goodsList.getSum()
   
