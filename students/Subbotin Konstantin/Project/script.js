53cc58bfb42d7606b6a131c0d01e50157d86cd9b
Konstantin Subbotin <subbotinkonstantin888@gmail.com>
2020-08-31 14:35:43 +0200

    Home work 2
    Home work 2. Update script.js

--- students/Subbotin Konstantin/Project/script.js
+++ students/Subbotin Konstantin/Project/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const goods = [
+/* const goods = [
     { title: 'Shirt', price: 150 },
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
@@ -13,7 +13,81 @@ const goods = [
   
   const renderGoodsList = (list) => {
     let goodsList = list.map(item => renderGoodsItem(item.title, item.price));
-    document.querySelector('.goods-list').innerHTML = goodsList;
+    document.querySelector('.goods-list').innerHTML = goodsList.join('');
   }
   
-  renderGoodsList(goods);
+  renderGoodsList(goods); */
+
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {
+        return `<div class="goods-item"><div class="item-img">image</div><h3>${this.title}</h3><p>${this.price}</p><button>Добавить</button></div>`;
+    }
+}
+
+
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [{
+                title: 'Shirt',
+                price: 150
+            },
+            {
+                title: 'Socks',
+                price: 50
+            },
+            {
+                title: 'Jacket',
+                price: 350
+            },
+            {
+                title: 'Shoes',
+                price: 250
+            },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml.join('');
+    }
+    //метод, определяющий суммарную стоимость всех товаров.
+    getSumPriceGoods() {
+        let sumPrice = 0;
+        for (let good in this.goods) {
+            sumPrice += good.price;
+        }
+        return sumPrice;
+    }
+}
+
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
+
+//пустые классы для корзины товаров и элемента корзины товаров.
+class Basket {
+    constructor() {
+        this.goods = [];
+    }
+    renderBasketItem() {}
+    addBasketItem() {}
+    removeBasketItem() {}
+    renderBasketSumPrice() {}
+}
+class BasketItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+    render() {}
+}
