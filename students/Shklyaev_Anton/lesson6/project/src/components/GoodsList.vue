1c1c7bb3c1a5d76c10ccab981ec0462191fec18d
Anton Shklyaev <unforgiven.91@mail.ru>
2020-09-17 00:34:34 +0400

    lesson6 выполнение дз

---
+++ students/Shklyaev_Anton/lesson6/project/src/components/GoodsList.vue
@@ -0,0 +1,30 @@
+<template>
+    <main>
+        <div v-if="goods.length" class="goods-list">
+            <div class="goods-item" v-for="item in goods" v-bind:key="item.id_product">
+                <h3>{{ item.product_name }}</h3>
+                <p>{{ item.price }}</p>
+                <button @click="addToBasket(item)">Buy</button>
+            </div>
+        </div>
+        <div v-else class="goods-list__empty">Нет данных</div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'GoodsList',
+    props: {
+        goods: Array,
+    },
+    methods: {
+        addToBasket(item) {
+            this.$emit("addToBasket", item);
+        }
+    }
+}
+</script>
+
+<style>
+/* */
+</style>
---
+++ students/Shklyaev_Anton/lesson6/project/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    
+    <slot/>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: {
+    basket: Array,
+  },
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    removeFromBasket(id) {
+      console.log("remove from basket", id);
+    },
+  },
+};
+</script>
+
+<style>
+/* */
+</style>
