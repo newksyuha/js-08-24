57714ae5f970c7f6e1527d310f28ebdb60aa8add
mlitta <i.m.litta@gmail.com>
2020-09-16 20:59:11 +0300

    Create style.css

---
+++ students/Natalya Stepanova/Lesson 4/style.css
@@ -0,0 +1,39 @@
+form {
+    width: 300px;
+    display: flex;
+    flex-wrap: wrap;
+}
+
+.phoneErr {
+    width: 100%;
+    color: red;
+}
+
+.nameErr {
+    width: 100%;
+    color: red;
+}
+
+.emailErr {
+    width: 100%;
+    color: red;
+}
+
+.red-border {
+    border: 2px solid red;
+}
+
+.hidden {
+    display: none;
+}
+
+button {
+    background-color: aqua;
+    border-radius: 5px;
+    border-color: #666;
+    border: solid;
+}
+
+button:hover {
+    background-color: grey;
+}
