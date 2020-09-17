69f32c27382746d9b8684ec7c04852a724a04455
Konstantin Subbotin <subbotinkonstantin888@gmail.com>
2020-09-17 14:44:17 +0200

    HW-6
    Added Vue components

---
+++ students/Subbotin Konstantin/Project/HV6_Vue/src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <div v-if="isBasketVisible" class="basket">
+            <div class="basket__item" v-for="basketItem in basket" v-bind:key="basketItem.id_product">
+                <h4>{{ basketItem.product_name }}</h4>
+                <p>{{ basketItem.price }}</p>
+                <button @click="removeFromBasket(basketItem.id_product)">Удалить</button>
+            </div>
+        </div>
+        <button type="button" class="cart-button" @click="isBasketVisible = !isBasketVisible">Корзина</button>
+        <input type="text" class="search" v-model="searchText" />
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    props: {
+        basket: Array,
+    },
+    data() {
+        return {
+            isBasketVisible: false,
+            searchText: '',
+        };
+    },
+    methods: {
+        removeFromBasket(id) {
+            console.log('remove from basket', id);
+        }
+    }
+};
+</script>
+
+<style>
+/* */
+</style>
