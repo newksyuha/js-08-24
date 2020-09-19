1d1d7458af09cdd4117e2c35c8031025fb1fafae
Ksu <newksyuha@gmail.com>
2020-09-19 15:22:47 +0300

    1/2 HWlesson6

---
+++ students/Oksana Kriutchenko/project/src/components/GoodsList.vue
@@ -0,0 +1,31 @@
+<template>
+     <main>
+            <div class="goods-list">
+                <div class="goods-item" v-for="item in goods" v-bind:key="item.id_product">
+                    <h3>{{ item.product_name }}</h3>
+                    <p>{{ item.price }}</p>
+                   <button @click="addToBasket(item)">Добавить</button>
+                </div>
+                <div v-if="!goods.length" class="goods-list-empty">
+                    <span>Нет данных</span>
+                  </div>
+            </div>
+        </main>
+</template>
+<script>
+export default {
+    name: 'GoodsList',
+    props: {
+        goods: Array,
+    },
+    methods: {
+        addToBasket(){
+        //
+      }
+    },
+}
+</script>    
+
+<style>
+/**/
+</style>
---
+++ students/Oksana Kriutchenko/project/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+     <header>
+            <input type="text" class="search" v-model="searchText" />
+            <button type="button" class="search-button">Искать</button>
+            <div v-if="isBasketVisible" class="basket">
+                <div class="basket__item" v-for="basketItem in basket" v-bind:key="basketItem.id_product" >
+                    <h4>{{ basketItem.product_name }}</h4>
+                    <p>{{ basketItem.price }}</p>
+                    <button @click="removeFromBasket(basketItem.id_product)">Удалить</button>
+                </div>
+            </div>
+            <button type="button" class="cart-button" @click="isBasketVisible = !isBasketVisible">Корзина</button>
+        </header>
+</template>
+<script>
+export default {
+    name: 'Header',
+    props: {
+        basket: Array,
+    },
+    data(){
+        return{
+        searchText: '',
+        isBasketVisible: false,
+        };
+    },
+    methods: {
+        removeFromBasket(id){
+           console.log('remove to basket', id);
+}
+    }
+};
+</script>
+<style>
+/**/ 
+</style>
