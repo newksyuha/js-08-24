8469de9e7678fdab8b7da2795e7e31764b16a53d
artemgorbunov7 <65342238+artemgorbunov7@users.noreply.github.com>
2020-09-21 14:44:33 +0400

    HW7

---
+++ students/GorbunovArtem/project/src/components/GoodsList.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <div v-if="goods.length" class="goods-list">
+            <div class="goods-item" v-for="item in goods" v-bind:key="item.id">
+                <h3>{{ item.name }}</h3>
+                <p>{{ item.price }}</p>
+                <button @click="$emit('add-item', item)">Buy</button>
+            </div>
+        </div>
+        <div v-else class="goods-list__empty">Нет данных</div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'GoodsList',
+    props: {
+        goods: Array,
+    },
+}
+</script>
+
+<style>
+/* */
+</style>
