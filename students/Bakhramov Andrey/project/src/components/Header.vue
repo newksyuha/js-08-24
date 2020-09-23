058024d244c05ffe920c1ca183ca381b499468d3
crazyadm <crazyadm@mail.ru>
2020-09-24 00:42:08 +0500

    Total

---
+++ students/Bakhramov Andrey/project/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <header>
+        <slot />
+        <button type="button" class="cart-button" @click="$emit('basket-toggle')">Корзина</button>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+    created() {
+        this.$eventBus.$on('button-click', this.handleButtonClick);
+    },
+    methods: {
+        handleButtonClick(payload) {
+            console.log('handleButtonClick', payload);
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ students/Bakhramov Andrey/project/src/components/Search.vue
@@ -0,0 +1,10 @@
+<template>
+    <input type="text" class="search" @input="$emit('input', $event.target.value)" :value="value" />
+</template>
+
+<script>
+export default {
+    name: 'Search',
+    props: ['value']
+}
+</script>
